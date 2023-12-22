2c2271558c427b80670c7d991cac043fc48319ea
Narek <narek.w7@gmail.com>
2023-12-22 18:18:14 +0400

    fix: minors

--- lib/application/theme/theme_cubit.dart
+++ lib/application/theme/theme_cubit.dart
@@ -9,7 +9,7 @@ class ThemeCubit extends Cubit<ThemeState> {
   ThemeCubit()
       : super(
           ThemeState(
-            colors: MDSColors(
+            colors: MDSColors.light(
               background: const Color(0xffF8F8F8),
               surface: const Color(0xffFFFFFF),
               primary: const Color(0xff0766EB),
@@ -52,6 +52,16 @@ class ThemeCubit extends Cubit<ThemeState> {
           bodySBold: MDSTypography.bodySBold,
           bodyXSRegular: MDSTypography.bodyXSRegular,
           bodyXSBold: MDSTypography.bodyXSBold,
+          headlineRegular: MDSTypography.headlineRegular,
+          headlineBold: MDSTypography.headlineBold,
+          title3Regular: MDSTypography.title3Regular,
+          title3Bold: MDSTypography.title3Bold,
+          title2Regular: MDSTypography.title2Regular,
+          title2Bold: MDSTypography.title2Bold,
+          title1Regular: MDSTypography.title1Regular,
+          title1Bold: MDSTypography.title1Bold,
+          titleLargeRegular: MDSTypography.titleLargeRegular,
+          titleLargeBold: MDSTypography.titleLargeBold,
         ), //TODO: default theme
         spacing: spacing,
       ),
