7154fa6ba75a301393da86c935110e78632085a8
TigranHayrapetyan13 <tigran.hayrapetyan17ht@gmail.com>
2023-12-26 09:33:11 +0400

    colors scheme

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,5 @@ import 'package:flutter/widgets.dart';
 import 'presentation/app/mds_widgetbook.dart';
 
 void main() {
- 
   runApp(const MDSWidgetbook());
 }
--- lib/presentation/theme/colors/colors.dart
+++ lib/presentation/theme/colors/colors.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:majestica_ds/majestica_ds.dart';
-import 'package:mds_widget_book/presentation/theme/colors/colors_editor/widgets/customizbale_color_scheme.dart';
+import 'package:mds_widget_book/presentation/theme/colors/widgets/customizbale_color_scheme.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(
   path: 'Theme',
-  name: 'Colors Editor',
+  name: 'Colors',
   type: MDSTheme,
 )
 Widget colors(BuildContext context) {
