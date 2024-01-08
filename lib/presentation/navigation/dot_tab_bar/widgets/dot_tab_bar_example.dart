1f1d47ae5ff77fdcf7a90496ba5c6b621d3aba0c
TigranHayrapetyan13 <tigran.hayrapetyan17ht@gmail.com>
2024-01-08 17:38:18 +0400

    fix:dot tab bar

--- lib/presentation/navigation/dot_tab_bar/widgets/dot_tab_bar_example.dart
+++ lib/presentation/navigation/dot_tab_bar/widgets/dot_tab_bar_example.dart
@@ -12,40 +12,23 @@ class DotTabBarExample extends StatefulWidget {
 }
 
 class _DotTabBarExampleState extends State<DotTabBarExample> {
-  int activeIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          if (activeIndex < 7) {
-            activeIndex += 1;
-          } else {
-            activeIndex = 0;
-          }
-        });
-      },
-      child: ExampleDisplay(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-          child: Column(
-            children: [
-              MDSDotTabBar(
-                items: [
-                  Container(
-                    color: Colors.red,
-                  ),
-                  Container(
-                    color: Colors.orange,
-                  ),
-                  Container(
-                    color: Colors.deepPurple,
-                  ),
-                ],
-              ),
-            ],
-          ),
+    return ExampleDisplay(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+        child: MDSDotTabBar(
+          items: [
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.orange,
+            ),
+            Container(
+              color: Colors.deepPurple,
+            ),
+          ],
         ),
       ),
     );
